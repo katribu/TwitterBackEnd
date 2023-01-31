e67871eebda41ebb61d51e50d2dd65d0a2cbe493
katb <katrinaburwash_17@hotmail.com>
2023-01-31 13:36:04 +0100

    added getTweetsbyUsername

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const {getTweets} = require('./services/database')
+const {getTweets, getTweetsByUsername} = require('./services/database')
 
 const express = require('express')
 const cors = require('cors')
@@ -19,6 +19,13 @@ app.get('/tweets', async(req,res)=>{
     //get tweets from database
     //respond with tweets as JSON
 })
+
+app.get('/tweets/:username', async(req,res)=>{
+    const {username} = req.params;
+    const tweets = await getTweetsByUsername(username)
+    res.json(tweets)
+})
+
 
 
 app.listen(PORT, () => console.log(`Twitter API listening on ${PORT}`))
--- services/database.js
+++ services/database.js
@@ -22,10 +22,32 @@ async function getTweets(){
         tweets.user_id = users.id
     ORDER BY created_at DESC;
     `); // this returns a promise on the rows that they match.
-    console.log(result)
+    // console.log(result)
     return result.rows;
 }
 
+async function getTweetsByUsername(username){
+    const result = await database.query(`
+    SELECT
+        tweets.id,
+        tweets.message,
+        tweets.created_at,
+        users.name,
+        users.username
+    FROM 
+        tweets
+    INNER JOIN users ON
+        tweets.user_id = users.id
+    WHERE
+        users.username = $1
+    ORDER BY created_at DESC;
+
+    `,[username]);
+    return result.rows
+}//$1 means prevents sequel injection and refers to the first item (index 0) in the array, which is the second argument in database.query
+
+
 module.exports = {
     getTweets,
+    getTweetsByUsername,
 }
